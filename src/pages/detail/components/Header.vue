78c828fe9b1720cc7c34e2a81f7a60fe434f3e02
Y <317553411@example.com>
2018-10-14 17:10:36 +0800

    travel

--- src/pages/detail/components/Header.vue
+++ src/pages/detail/components/Header.vue
@@ -59,14 +59,13 @@ export default {
     width .8rem
     height .8rem
     line-height .8rem
-    border-radius .8rem
+    border-radius .4rem
     text-align center
     background rgba(0, 0, 0, .8)
     .header-abs-back
       color #fff
       font-size .4rem
   .header-fixed
-    z-index 2
     position fixed
     top 0
     left 0
